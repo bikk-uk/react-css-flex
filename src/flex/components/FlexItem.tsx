baae83496d9939f934fcbae0681436023083776d
aboyce <adam@aboyce.uk>
2020-06-19 18:06:07 +0100

    Cleaned up flex item props.

--- src/flex/components/FlexItem.tsx
+++ src/flex/components/FlexItem.tsx
@@ -4,11 +4,7 @@ import React from 'react'
 // Types
 import type { DivProps } from '../index'
 
-export interface Props extends DivProps {
-  test?: string
-}
-
-function Item({ children, style = {}, ...rest }: Props): React.ReactElement {
+function Item({ children, style = {}, ...rest }: DivProps): React.ReactElement {
   return (
     <div style={{ ...style }} {...rest}>
       {children}
