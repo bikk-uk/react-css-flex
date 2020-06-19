2ea9032c7c09fafbd0245bf54b8a9e367445eb8f
aboyce <adam@aboyce.uk>
2020-06-19 17:23:40 +0100

    Added justify-content, align-items, align-content, and flex-flow support.

--- src/flex/components/Flex.tsx
+++ src/flex/components/Flex.tsx
@@ -6,49 +6,66 @@ import {
   JustifyContentProperty,
   AlignItemsProperty,
   AlignContentProperty,
-  FlexFlowProperty,
 } from 'csstype'
 
-// Types
-import type { DivProps } from '../index'
-
-export interface Props extends DivProps {
-  // 'display'
-  inline?: boolean
-  // 'flex-direction' short
-  row?: boolean
-  rowReverse?: boolean
-  column?: boolean
-  columnReverse?: boolean
-  // 'flex-direction' manual
-  flexDirection?: FlexDirectionProperty
-  // 'flex-wrap' short
-  wrap?: boolean
-  noWrap?: boolean
-  wrapReverse?: boolean
-  // 'flex-wrap' manual
-  flexWrap?: FlexWrapProperty
-}
+// Props
+import { FlexProps } from './index'
 
 function Flex({
-  // display
+  // 'display'
   inline,
-  // flex-direction
+
+  // 'flex-direction' short
   row,
   rowReverse,
   column,
   columnReverse,
+  // 'flex-direction' manual
   flexDirection,
-  // flex-wrap
+
+  // 'flex-wrap' short
   wrap,
   noWrap,
   wrapReverse,
+  // 'flex-wrap' manual
   flexWrap,
+
+  // 'justify-content' short
+  justifyStart,
+  justifyEnd,
+  justifyCenter,
+  justifySpaceBetween,
+  justifySpaceAround,
+  // 'justify-content' manual
+  justifyContent,
+
+  // 'align-items' short
+  alignItemsStart,
+  alignItemsEnd,
+  alignItemsCenter,
+  alignItemsBaseline,
+  alignItemsStretch,
+  // 'align-items' manual
+  alignItems,
+
+  // 'align-content' short
+  alignContentStart,
+  alignContentEnd,
+  alignContentCenter,
+  alignContentSpaceBetween,
+  alignContentSpaceAround,
+  alignContentStretch,
+  // 'align-content' manual
+  alignContent,
+
+  // 'flex-flow'
+  flow,
+
   // required
   style = {},
   children,
   ...rest
-}: Props): React.ReactElement {
+}: FlexProps): React.ReactElement {
   const parseDisplay = React.useCallback(
     (): React.CSSProperties => ({
       display: inline ? 'inline-flex' : 'flex',
@@ -79,12 +96,69 @@ function Flex({
     return value ? { flexWrap: value } : {}
   }, [flexWrap, noWrap, wrap, wrapReverse])
 
+  const parseJustifyContent = React.useCallback(() => {
+    // the manual version has been provided, that takes precedence
+    if (justifyContent) return { justifyContent }
+    // see if a specific value has been provided, first come first serve
+    let value: JustifyContentProperty | null = null
+    if (justifyStart) value = 'flex-start'
+    else if (justifyEnd) value = 'flex-end'
+    else if (justifyCenter) value = 'center'
+    else if (justifySpaceBetween) value = 'space-between'
+    else if (justifySpaceAround) value = 'space-around'
+    return value ? { justifyContent: value } : {}
+  }, [justifyContent, justifyStart, justifyEnd, justifyCenter, justifySpaceBetween, justifySpaceAround])
+
+  const parseAlignItems = React.useCallback(() => {
+    // the manual version has been provided, that takes precedence
+    if (alignItems) return { alignItems }
+    // see if a specific value has been provided, first come first serve
+    let value: AlignItemsProperty | null = null
+    if (alignItemsStart) value = 'flex-start'
+    if (alignItemsEnd) value = 'flex-end'
+    if (alignItemsCenter) value = 'center'
+    if (alignItemsBaseline) value = 'baseline'
+    if (alignItemsStretch) value = 'stretch'
+    return value ? { alignItems: value } : {}
+  }, [alignItems, alignItemsStart, alignItemsEnd, alignItemsCenter, alignItemsBaseline, alignItemsStretch])
+
+  const parseAlignContent = React.useCallback(() => {
+    // the manual version has been provided, that takes precedence
+    if (alignContent) return { alignContent }
+    // see if a specific value has been provided, first come first serve
+    let value: AlignContentProperty | null = null
+    if (alignContentStart) value = 'flex-start'
+    if (alignContentEnd) value = 'flex-end'
+    if (alignContentCenter) value = 'center'
+    if (alignContentSpaceBetween) value = 'space-between'
+    if (alignContentSpaceAround) value = 'space-around'
+    if (alignContentStretch) value = 'stretch'
+    return value ? { alignContent: value } : {}
+  }, [
+    alignContent,
+    alignContentStart,
+    alignContentEnd,
+    alignContentCenter,
+    alignContentSpaceBetween,
+    alignContentSpaceAround,
+    alignContentStretch,
+  ])
+
+  const parseFlexFlow = React.useCallback(() => {
+    // the manual version has been provided, that takes precedence
+    return flow ? { flexFlow: flow } : {}
+  }, [flow])
+
   return (
     <div
       style={{
         ...parseDisplay(),
         ...parseFlexDirection(),
         ...parseFlexWrap(),
+        ...parseJustifyContent(),
+        ...parseAlignItems(),
+        ...parseAlignContent(),
+        ...parseFlexFlow(),
         ...style,
       }}
       {...rest}>
--- src/flex/tests/Flex.test.tsx
+++ src/flex/tests/Flex.test.tsx
@@ -8,7 +8,7 @@ import { matchesSnapshot } from './helpers/snapshots'
 // Tested Module
 import Flex from '../components/Flex'
 
-describe('<Flex />', () => {
+describe('<Flex /> - General', () => {
   it('renders an empty flex container', async () => {
     const component = <Flex />
     const snapshot = `
@@ -71,7 +71,9 @@ describe('<Flex />', () => {
     fireEvent.click(await findByTestId('flex'))
     expect(onClick).toHaveBeenCalled()
   })
+})
 
+describe('<Flex /> - flex-direction', () => {
   it('allows the flex-direction row to be set', async () => {
     const component = <Flex row />
     const snapshot = `
@@ -122,7 +124,7 @@ describe('<Flex />', () => {
     matchesSnapshot(component, snapshot)
   })
 
-  test('flex-direction when manually set overrides simple props', async () => {
+  test('flex-direction when manually set, overrides simple props', async () => {
     const component = <Flex flexDirection='column' row rowReverse />
     const snapshot = `
   <div
@@ -131,7 +133,9 @@ describe('<Flex />', () => {
   `
     matchesSnapshot(component, snapshot)
   })
+})
 
+describe('<Flex /> - flex-wrap', () => {
   it('allows the flex-wrap wrap to be set', async () => {
     const component = <Flex wrap />
     const snapshot = `
@@ -162,11 +166,219 @@ describe('<Flex />', () => {
     matchesSnapshot(component, snapshot)
   })
 
-  test('flex-wrap when manually set overrides simple props', async () => {
+  test('flex-wrap when manually set, overrides simple props', async () => {
     const component = <Flex flexWrap='wrap' noWrap wrapReverse />
     const snapshot = `
   <div
     style="display: flex; flex-wrap: wrap;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+})
+
+describe('<Flex /> - justify-content', () => {
+  it('allows the justify-content flex-start to be set', async () => {
+    const component = <Flex justifyStart />
+    const snapshot = `
+  <div
+    style="display: flex; justify-content: flex-start;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the justify-content flex-end to be set', async () => {
+    const component = <Flex justifyEnd />
+    const snapshot = `
+  <div
+    style="display: flex; justify-content: flex-end;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the justify-content center to be set', async () => {
+    const component = <Flex justifyCenter />
+    const snapshot = `
+  <div
+    style="display: flex; justify-content: center;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the justify-content space-between to be set', async () => {
+    const component = <Flex justifySpaceBetween />
+    const snapshot = `
+  <div
+    style="display: flex; justify-content: space-between;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the justify-content space-around to be set', async () => {
+    const component = <Flex justifySpaceAround />
+    const snapshot = `
+  <div
+    style="display: flex; justify-content: space-around;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  test('justify-content when manually set, overrides simple props', async () => {
+    const component = <Flex justifyContent='center' justifyEnd justifySpaceAround />
+    const snapshot = `
+  <div
+    style="display: flex; justify-content: center;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+})
+
+describe('<Flex /> - align-items', () => {
+  it('allows the align-items flex-start to be set', async () => {
+    const component = <Flex alignItemsStart />
+    const snapshot = `
+  <div
+    style="display: flex; align-items: flex-start;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the align-items flex-end to be set', async () => {
+    const component = <Flex alignItemsEnd />
+    const snapshot = `
+  <div
+    style="display: flex; align-items: flex-end;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the align-items center to be set', async () => {
+    const component = <Flex alignItemsCenter />
+    const snapshot = `
+  <div
+    style="display: flex; align-items: center;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the align-items baseline to be set', async () => {
+    const component = <Flex alignItemsBaseline />
+    const snapshot = `
+  <div
+    style="display: flex; align-items: baseline;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the align-items stretch to be set', async () => {
+    const component = <Flex alignItemsStretch />
+    const snapshot = `
+  <div
+    style="display: flex; align-items: stretch;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  test('align-items when manually set, overrides simple props', async () => {
+    const component = <Flex alignItems='center' alignItemsBaseline alignItemsStretch />
+    const snapshot = `
+  <div
+    style="display: flex; align-items: center;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+})
+
+describe('<Flex /> - align-content', () => {
+  it('allows the align-content flex-start to be set', async () => {
+    const component = <Flex alignContentStart />
+    const snapshot = `
+  <div
+    style="display: flex; align-content: flex-start;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the align-content flex-end to be set', async () => {
+    const component = <Flex alignContentEnd />
+    const snapshot = `
+  <div
+    style="display: flex; align-content: flex-end;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the align-content center to be set', async () => {
+    const component = <Flex alignContentCenter />
+    const snapshot = `
+  <div
+    style="display: flex; align-content: center;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the align-content space-between to be set', async () => {
+    const component = <Flex alignContentSpaceBetween />
+    const snapshot = `
+  <div
+    style="display: flex; align-content: space-between;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the align-content space-around to be set', async () => {
+    const component = <Flex alignContentSpaceAround />
+    const snapshot = `
+  <div
+    style="display: flex; align-content: space-around;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  it('allows the align-content stretch to be set', async () => {
+    const component = <Flex alignContentStretch />
+    const snapshot = `
+  <div
+    style="display: flex; align-content: stretch;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+
+  test('align-content when manually set, overrides simple props', async () => {
+    const component = <Flex alignContent='center' alignContentEnd alignContentStart />
+    const snapshot = `
+  <div
+    style="display: flex; align-content: center;"
+  />
+  `
+    matchesSnapshot(component, snapshot)
+  })
+})
+
+describe('<Flex /> - flex-flow', () => {
+  it('allows flex-flow to be set', async () => {
+    const component = <Flex flow='row nowrap' />
+    const snapshot = `
+  <div
+    style="display: flex; flex-flow: row nowrap;"
   />
   `
     matchesSnapshot(component, snapshot)
