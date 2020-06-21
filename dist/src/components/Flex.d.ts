ef5f844b5e27306e53ba8b09fca1deebb982dc1c
aboyce <adam@aboyce.uk>
2020-06-21 16:38:40 +0100

    Prevent tests from being transpiled, rebuild dist.

---
+++ dist/src/components/Flex.d.ts
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FlexProps } from './index'
+declare function Flex({
+  inline,
+  row,
+  rowReverse,
+  column,
+  columnReverse,
+  flexDirection,
+  wrap,
+  noWrap,
+  wrapReverse,
+  flexWrap,
+  justifyStart,
+  justifyEnd,
+  justifyCenter,
+  justifySpaceBetween,
+  justifySpaceAround,
+  justifyContent,
+  alignItemsStart,
+  alignItemsEnd,
+  alignItemsCenter,
+  alignItemsBaseline,
+  alignItemsStretch,
+  alignItems,
+  alignContentStart,
+  alignContentEnd,
+  alignContentCenter,
+  alignContentSpaceBetween,
+  alignContentSpaceAround,
+  alignContentStretch,
+  alignContent,
+  flow,
+  style,
+  children,
+  ...rest
+}: FlexProps): React.ReactElement
+export default Flex
--- dist/src/flex/tests/helpers/snapshots.d.ts
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export declare const matchesSnapshot: (input: React.ReactElement, snapshot: string) => void;
