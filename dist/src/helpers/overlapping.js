f01c5a14769f2c90469b540181a88b827d65e087
aboyce <adam@aboyce.uk>
2020-06-27 19:06:41 +0100

    Rebuilt dist.

--- dist/src/helpers/overlapping.js
+++ dist/src/helpers/overlapping.js
@@ -4,12 +4,12 @@ exports.checkOverlapping = void 0;
 exports.checkOverlapping = (propName, ...props) => {
     let foundOne = false;
     props.forEach((prop) => {
-        if (prop && !foundOne) {
+        if (prop) {
+            if (foundOne) {
+                console.warn(`[@react-css/flex] Multiple values have been provided for ${propName}.`);
+                return;
+            }
             foundOne = true;
-        }
-        else if (prop && foundOne) {
-            console.warn(`[@react-css/flex] Multiple values have been provided for ${propName}.`);
-            return;
         }
     });
 };
