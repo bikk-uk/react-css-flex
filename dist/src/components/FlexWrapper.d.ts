6f83a1bcb20786a04a03ccb86896fe1269d4191a
aboyce <adam@aboyce.uk>
2020-07-05 16:16:59 +0100

    v1.1.2

--- dist/src/components/FlexWrapper.d.ts
+++ dist/src/components/FlexWrapper.d.ts
@@ -1,5 +1,5 @@
 import React from 'react'
-import type { FlexContainerProps } from '../../index'
+import type { FlexContainerProps } from '../index'
 import FlexItem from './FlexItem'
 export declare class Flex extends React.Component<FlexContainerProps> {
   static Item: typeof FlexItem
