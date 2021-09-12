e15488c1f2316cf4ee5919e253fdc4012752b134
aboyce <adam@aboyce.uk>
2021-09-12 17:36:56 +0100

    Updated dependencies, updated test config, swapped deprecated config.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
